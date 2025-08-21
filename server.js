ba2838e5cdca1dd079de094234bd96f1ce4d9f90
veeralakshmi51 <karthigaraj0928@gmail.com>
2025-08-21 22:21:52 +0530

    on

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
